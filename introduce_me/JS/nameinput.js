194258122a46bee4939f43e162da6a2ec2a65337
songyi-p <songyi7091@gmail.com>
2022-02-09 14:49:49 +0900

    second modify

--- introduce_me/JS/nameinput.js
+++ introduce_me/JS/nameinput.js
@@ -1,5 +1,6 @@
 const nameForm = document.getElementById("name-form")
 const nameInput = document.querySelector("#name-form input")
+const nameButton = document.querySelector("#name-form button")
 const inputByCom = document.getElementById("input-by-com")
 
 nameForm.addEventListener("submit", submitEvent)
@@ -9,4 +10,5 @@ function submitEvent (event) {
     const nameValue = nameInput.value;
     inputByCom.innerText = nameValue
     nameInput.value = "";
+    inputByCom.scrollIntoView();
 }
